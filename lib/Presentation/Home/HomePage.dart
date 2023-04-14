d2133d08368634c54ce910cc9b4f950b54d022ab
Rohan Agarwal <rohandiwan1824@gmail.com>
2023-04-14 17:08:22 +0530

    UI Adaptability in Drawer complete

--- lib/Presentation/Home/HomePage.dart
+++ lib/Presentation/Home/HomePage.dart
@@ -3,10 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:ftest/Presentation/Participants/Participants.dart';
-import 'package:ftest/Presentation/Scanner/Scanner.dart';
 import 'package:ftest/Widgets/EventCard.dart';
-
 
 import '../../Widgets/AppDrawer.dart';
 
@@ -15,54 +12,60 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //Navigator.pop(context);
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Home',
-          style: TextStyle(color: Colors.black),),
-          iconTheme: IconThemeData(color: Colors.black),
+      appBar: AppBar(
+        title: Text(
+          'Home',
+          style: TextStyle(color: Colors.black),
         ),
-        drawer: Drawer(
-          backgroundColor: Colors.black,
-          child:  AppDrawer(fAuth: FirebaseAuth.instance),
-        ),
-        body: Scaffold(
-          body: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: MediaQuery.of(context).size.height * 1,
-              child: StreamBuilder(
-                stream: FirebaseFirestore.instance.collection('Event')
-                .where('coordinators', arrayContains: FirebaseAuth.instance.currentUser!.email)
-                .snapshots(),
-                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(child: CircularProgressIndicator());
-                  } else if (!snapshot.hasData) {
-                    return Container();
-                  } else if (snapshot.hasData) {
-                    return ListView(
-                        children: snapshot.data!.docs.map((e) {
-                          return e['endTime'].toDate().isAfter(DateTime.now())? EventCard(
-                              endTime: e['endTime'].toDate(),
-                              imageUrl: e['backDrop'],
-                              eventName: e['eventName'],
-                              departName :e['deptName'],
-                              venue: e['venue'],
-                              dateTime: e['startTime'].toDate(),
-                              id: e['eventID'],
-                              page: 'history',
-                              ): SizedBox();
-                    }).toList());
-                  } else {
-                    return Container();
-                  }
-                },
-              ),
+        iconTheme: IconThemeData(color: Colors.black),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.black,
+        child: AppDrawer(fAuth: FirebaseAuth.instance, pageTitle: "Home",),
+      ),
+      body: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            height: MediaQuery.of(context).size.height * 1,
+            child: StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection('Event')
+                  .where('coordinators',
+                      arrayContains: FirebaseAuth.instance.currentUser!.email)
+                  .snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (!snapshot.hasData) {
+                  return Container();
+                } else if (snapshot.hasData) {
+                  return ListView(
+                      children: snapshot.data!.docs.map((e) {
+                    return e['endTime'].toDate().isAfter(DateTime.now())
+                        ? EventCard(
+                            endTime: e['endTime'].toDate(),
+                            imageUrl: e['backDrop'],
+                            eventName: e['eventName'],
+                            departName: e['deptName'],
+                            venue: e['venue'],
+                            dateTime: e['startTime'].toDate(),
+                            id: e['eventID'],
+                            page: 'history',
+                          )
+                        : SizedBox();
+                  }).toList());
+                } else {
+                  return Container();
+                }
+              },
             ),
           ),
         ),
-        //)
+      ),
+      //)
     );
   }
 }
--- lib/Widgets/AppDrawer.dart
+++ lib/Widgets/AppDrawer.dart
@@ -2,13 +2,15 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:ftest/Presentation/History/History.dart';
+import 'package:ftest/Presentation/Home/HomePage.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../InfraStructure/AuthRepo.dart';
 import '../Presentation/Authentication/Login.dart';
 
 class AppDrawer extends StatefulWidget {
   FirebaseAuth fAuth;
-  AppDrawer({super.key, required this.fAuth});
+  String pageTitle;
+  AppDrawer({super.key, required this.fAuth, required this.pageTitle});
   @override
   State<StatefulWidget> createState() => _AppDrawerState();
 }
@@ -16,15 +18,16 @@ class AppDrawer extends StatefulWidget {
 class _AppDrawerState extends State<AppDrawer> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
+    return Center(
       child: Container(
-        color: const Color( 0xffe6e6e6 ),
-        child: Column(
-          children: [
-            Container(
+          color: const Color(0xffe6e6e6),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
                 width: double.infinity,
                 height: 300,
-                padding: const EdgeInsets.only(top: 35.0, bottom: 5.0),
+                padding: EdgeInsets.only(top: 35, bottom: 5.0),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
@@ -33,61 +36,97 @@ class _AppDrawerState extends State<AppDrawer> {
                           widget.fAuth.currentUser!.photoURL.toString()),
                       maxRadius: 100,
                     ),
-                    Text(FirebaseAuth.instance.currentUser!.displayName.toString(),style: GoogleFonts.inter(),),
-                    Text(FirebaseAuth.instance.currentUser!.email.toString(), style: GoogleFonts.inter(color: Color(0xff616161)),),
+                    Text(
+                      FirebaseAuth.instance.currentUser!.displayName.toString(),
+                      style: GoogleFonts.inter(),
+                    ),
+                    Text(
+                      FirebaseAuth.instance.currentUser!.email.toString(),
+                      style: GoogleFonts.inter(color: Color(0xff616161)),
+                    ),
                   ],
                 ),
               ),
-            Center(
-              child: SizedBox(
-                height: 25.0,
-                width: MediaQuery.of(context).size.width * 0.7,
-                child: const Divider(
-                    color: Color(0xffa7a7a7),
-                  thickness: 1.0,
-                )
+              Center(
+                child: SizedBox(
+                    height: 25,
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    child: const Divider(
+                      color: Color(0xffa7a7a7),
+                      thickness: 1.0,
+                    )),
               ),
-            ),
-            Container(
-                padding: const EdgeInsets.all(5.0),
-                color: const Color(0xffd9d9d9),
-                child: ListTile(
-                  onTap: (){
-                    Navigator.of(context).push(MaterialPageRoute(
-                                          builder: (context) => History(),
-                                        ));
-                  },
-                  leading: Icon(Icons.history),
-                  title: Text("History", style: GoogleFonts.inter(fontSize: 20.0, fontWeight: FontWeight.w400),),
-                  // onTap: , this will open the home page
-                )
-            ),
-            SizedBox(height: MediaQuery.of(context).size.height * 0.4),
-            Container(
-              margin: const EdgeInsets.only(bottom: 20.0),
-              width: MediaQuery.of(context).size.width * 0.6,
-              decoration: BoxDecoration(border: Border.all(color: Color(0xffb91c1c),)),
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: TextButton(
-                  child: Text(
-                    "Sign Out",
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.inter(
-                        color: Color(0xffb91c1c),
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.normal),
+              Container(
+                  padding: const EdgeInsets.all(5.0),
+                  color: const Color(0xffd9d9d9),
+                  child: ListTile(
+                    onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const History(),
+                    )),
+                    leading: const Icon(Icons.history),
+                    title: Text(
+                      "History",
+                      style: GoogleFonts.inter(
+                          fontSize: 20.0, fontWeight: FontWeight.w400),
+                    ),
+                  )),
+              Visibility(
+                  visible: widget.pageTitle != "Home",
+                  child: Container(
+                      padding: const EdgeInsets.all(5.0),
+                      color: const Color(0xffd9d9d9),
+                      child: ListTile(
+                        onTap: () {
+                          Navigator.pop(context);
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const HomePage()));
+                        },
+                        leading: const Icon(Icons.history),
+                        title: Text(
+                          "Home",
+                          style: GoogleFonts.inter(
+                              fontSize: 20.0, fontWeight: FontWeight.w400),
+                        ),
+                      )
+                  )
+              ),
+              Expanded(
+                  child: Container(
+                    constraints: BoxConstraints(
+                      minHeight: MediaQuery.of(context).size.height * 0.25,
+                      maxHeight: MediaQuery.of(context).size.height * 0.45,
+                    ),
+                    alignment: Alignment.bottomCenter,
                   ),
-                  onPressed: () async {
-                    await AuthRepo.signOut().whenComplete(() => print("completes"));
-                    SchedulerBinding.instance.addPostFrameCallback((_) => Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => const Login())));
-                  },
-                ),
+              ), //this is the empty space between the button and the list items
+              Container(
+                  margin: const EdgeInsets.only(bottom: 20.0),
+                  width: 200,
+                  decoration: BoxDecoration(border: Border.all(color: const Color(0xffb91c1c))),
+                  child: TextButton(
+                    child: Text(
+                      "Sign Out",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.inter(
+                          color: const Color(0xffb91c1c),
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.normal),
+                    ),
+                    onPressed: () async {
+                      await AuthRepo.signOut()
+                          .whenComplete(() => print("completes"));
+                      SchedulerBinding.instance.addPostFrameCallback(
+                              (_) => Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                  builder: (context) => const Login())));
+                    },
+                  )
               ),
-            ),
-          ],
-        )
-      )
+            ],
+          )
+      ),
     );
   }
 }
