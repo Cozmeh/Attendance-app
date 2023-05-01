aed9058b005196eaf08567df8f1ff7c259ae00de
Cozmeh <hemsainath15@gmail.com>
2023-05-01 21:58:32 +0530

    Added splash screen

---
+++ lib/SplashScreen.dart
@@ -0,0 +1,51 @@
+// ignore_for_file: file_names
+import 'package:flutter/material.dart';
+import 'package:ftest/MainHome.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    _gotoHome();
+  }
+
+  _gotoHome() async {
+    await Future.delayed(const Duration(milliseconds: 1500));
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const Home(),
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.25,
+              child: Image.asset('assets/kjcLogo.png'),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.02,
+            ),
+            const Text(
+              "KJC AMS",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
